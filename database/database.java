42b71cdfc2f97dcc4c7da36d12b2095e869f35f2
fsociety <143713077+fsociety1001@users.noreply.github.com>
2023-10-07 21:46:02 -0300

    database.java

---
+++ database/database.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class BancoDados {
+
+    public static void main(String[] args) throws SQLException {
+
+        System.out.println("Banco de dados");
+
+        String dsn = "jdbc:mysql://localhost/localhome";
+        Connection connection = DriverManager.getConnection(dsn, "fsociety", "fsociety");
+
+        String sql = "INSERT INTO contatos (nome, email, endereco, dataNascimento) values('fsociety', 'fsociey', 'bariro BARRO BRANCO', '2023/10/07')";
+
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        statement.execute();
+        statement.close();
+
+    }
+
+}
